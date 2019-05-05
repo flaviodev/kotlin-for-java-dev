9b9a44efa740177aada9579eeadcfa8bc50117b3
flaviodev <fdsdev@gmail.com>
2019-05-05 14:59:22 -0300

    nullable types

--- src/main/kotlin/com/flaviodev/kfjd/_05nullability/NullableTypesUnderHood.kt
+++ src/main/kotlin/com/flaviodev/kfjd/_05nullability/NullableTypesUnderHood.kt
@@ -1,13 +1,13 @@
 package com.flaviodev.kfjd._05nullability
 
+val s1: String = "always not null"
+val s2: String? = "can be null or non-null"
+
+val s3: String? = null
+
+val length: Int? = s3?.length
+
 fun main() {
-
-    val s1: String = "always not null"
-    val s2: String? = "can be null or non-null"
-
-    val s3: String? = null
-
-    val length: Int? = s3?.length
 
     if(s3 != null)
         println(s3.length)
