48b919fa9c63b4c56ad2e4320f7483168f849cae
flaviodev <fdsdev@gmail.com>
2019-05-06 17:04:21 -0300

    playground safe cast

--- src/main/kotlin/com/flaviodev/kfjd/_07functionalprogramming/Lambdas.kt
+++ src/main/kotlin/com/flaviodev/kfjd/_07functionalprogramming/Lambdas.kt
@@ -4,6 +4,5 @@ fun main() {
     val map = mapOf(1 to "One", 2 to "Two", 3 to "Three", 4 to "Four")
     map.mapValues { println("${it.key} => ${it.value}") }
     map.mapValues { (key, value) -> println("$key => $value") }
-    map.mapValues { (_, value) -> println("$value") }
-
+    map.mapValues { (_, value) -> println(value) }
 }
