ef8446449dd82f3ef3a214e615bf35b7ef945af1
flaviodev <fdsdev@gmail.com>
2019-05-12 12:28:24 -0300

    common operations on collections

--- src/main/java/com/flaviodev/InvocationFromJava.java
+++ src/main/java/com/flaviodev/InvocationFromJava.java
@@ -3,11 +3,75 @@ package com.flaviodev;
 import com.flaviodev.kfjd._02basics.Math;
 import com.flaviodev.kfjd._02basics.NamedAndDefaultKt;
 
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.TreeMap;
+
 public class InvocationFromJava {
 
     public static void main(String[] args) {
-        Math.printFirstAndSecondValuesAndMaxBetweenThem(1,3,Math.max(1,3));
+       // Math.printFirstAndSecondValuesAndMaxBetweenThem(1, 3, Math.max(1, 3));
 
-        NamedAndDefaultKt.displaySeparator('#',5);
+       // NamedAndDefaultKt.displaySeparator('#', 5);
+
+        Pessoa flavio = new Pessoa("1", "Flavio");
+        Pessoa jether = new Pessoa("1", "Jether");
+
+
+        HashSet<Pessoa> pessoas = new HashSet<>();
+
+        pessoas.add(flavio);
+        pessoas.add(jether);
+        System.out.println(pessoas);
+
+        TreeMap<String, Pessoa> map = new TreeMap<>();
+
+    }
+
+}
+
+class Pessoa {
+    private String id;
+    private String name;
+
+    public Pessoa(String id, String name) {
+        this.setId(id);
+        this.setName(name);
+    }
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pessoa pessoa = (Pessoa) o;
+        return Objects.equals(id, pessoa.id);
+    }
+
+    @Override
+    public int hashCode() {
+        //return Objects.hash(id);
+        return 1;
+    }
+
+    @Override
+    public String toString() {
+        return getId() + " - " + getName();
     }
 }
---
+++ src/main/kotlin/com/flaviodev/kfjd/_07functionalprogramming/CommonOperationsOnCollections.kt
@@ -0,0 +1,64 @@
+package com.flaviodev.kfjd._07functionalprogramming
+
+
+fun main() {
+    val list = listOf(1, 2, 3, 4, 5, 6, 7, 8)
+
+    println("Numbers' list -> $list")
+
+    val evenList = list.filter { it % 2 == 0 }
+    val oddList = list.filter { it % 2 != 0 }
+
+    println("Even numbers filtered from list -> $evenList")
+    println("Odd numbers filtered from list -> $oddList")
+
+    println("Numbers multiplied by two -> ${list.map { it * 2 }}")
+
+    println("List as reversed -> ${list.asReversed()}")
+
+    println("All numbers are grater than zero? -> ${list.all { it > 0 }}")
+    println("None of numbers are less than one? -> ${list.none { it < 1 }}")
+
+    println("List has any number? -> ${list.any()}")
+    println("List has any number grater than four? -> ${list.any { it > 4 }}")
+
+    println("First element from list -> ${list.first()}")
+    println("Last element from list -> ${list.last()}")
+    println("First element or null from filtered list -> ${list.firstOrNull { it < 0 }}")
+
+    println("Find first element grater than two -> ${list.find { it > 2 }}")
+    println("Find last element less than seven -> ${list.findLast { it < 7 }}")
+    println("Last index from list -> ${list.lastIndex}")
+    println("Count elements from filtered list -> ${list.filter { it % 2 == 0 }.count()}")
+    println("Count elements with condition from list -> ${list.count { it % 2 == 0 }}")
+
+    val (even, odd) = list.partition { it % 2 == 0 }
+    println("Even numbers from partitioned list -> $even")
+    println("Even numbers from partitioned list -> $odd")
+
+    val persons = listOf(Person(name = "Alice", age = 31),
+            Person(name = "Bob", age = 29),
+            Person(name = "Carol", age = 31))
+
+    println("Grouping persons by age -> ${persons.groupBy { it.age }}")
+    println("Associate persons by name -> ${persons.associateBy { it.name }}")
+    println("Associate persons by age (removing duplicates) -> ${persons.associateBy { it.age }}")
+    println("Associate numbers of list to a letter -> ${list.associateBy { 'a' + it - 1 }}")
+    println("Associate numbers of list to a letter -> ${list.associateBy { 'A' + it - 1 }}")
+
+    println("Associate numbers of list to a letter -> ${list.associate { ('A' + it-1).toString() + it to it * 100 }}")
+
+    println("Zipping numbers with persons -> ${list.zip(persons)}")
+    println("Zip with the next number -> ${list.zipWithNext()}")
+
+    val list2 = listOf(9,10,11)
+    val list3 = listOf(12,13,14,15)
+
+    val listOfLists = listOf(list,list2,list3)
+
+    println("List of lists -> $listOfLists")
+    println("Flatten of List of lists -> ${listOfLists.flatten()}")
+
+}
+
+data class Person(val name: String, val age: Int)
