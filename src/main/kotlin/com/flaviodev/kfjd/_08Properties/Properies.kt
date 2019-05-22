f9c513ca682b5130b26d8bdc5db4649d2ed52f57
flaviodev <fdsdev@gmail.com>
2019-05-22 09:45:17 -0300

    lesson 1 week 4 - properties

--- src/main/kotlin/com/flaviodev/kfjd/_08Properties/Properies.kt
+++ src/main/kotlin/com/flaviodev/kfjd/_08Properties/Properies.kt
@@ -1,9 +1,22 @@
 package com.flaviodev.kfjd._08Properties
 
 fun main() {
-    val person = Person(name = "John")
+    val person = Person(name = "John", age = 22, gender = Gender.MALE)
 
-    println("The name of the person is ${person.name}")
+    val message ="The name of the person is ${person.name}, ${person.pronoun()}" +
+            " is ${person.age} then ${person.pronoun()} ${if (person.isAgeOver18) "is" else "not is"} over 18"
+
+    println(message)
 }
 
-class Person(val name: String)
+class Person(val name: String, val age: Int, val gender: Gender) {
+    val isAgeOver18: Boolean
+        get() = age >= 18
+}
+
+enum class Gender {
+    MALE,
+    FEMALE
+}
+
+fun Person.pronoun(): String = if (gender == Gender.MALE) "he" else "she"
