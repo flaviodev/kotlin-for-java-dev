6908e660b7920b9180b2cfe44462270c2ff952cb
flaviodev <fdsdev@gmail.com>
2019-05-22 21:50:48 -0300

    lesson 1 week 4 - properties

--- src/main/kotlin/com/flaviodev/kfjd/_08Properties/Properies.kt
+++ src/main/kotlin/com/flaviodev/kfjd/_08Properties/Properies.kt
@@ -1,17 +1,28 @@
 package com.flaviodev.kfjd._08Properties
 
 fun main() {
+
     val person = Person(name = "John", age = 22, gender = Gender.MALE)
+    person.print()
 
-    val message ="The name of the person is ${person.name}, ${person.pronoun()}" +
-            " is ${person.age} then ${person.pronoun()} ${if (person.isAgeOver18) "is" else "not is"} over 18"
-
-    println(message)
+    val person2 = Person(name = "John", age = 22, gender = Gender.MALE, description = "tall and black hair")
+    person2.print()
 }
 
 class Person(val name: String, val age: Int, val gender: Gender) {
+
+    // defining set private
+    var description: String? = null
+        private set
+
+    // creating get like a property
     val isAgeOver18: Boolean
         get() = age >= 18
+
+    constructor(name: String, age: Int, gender: Gender, description: String) :
+            this(name = name, age = age, gender = gender) {
+        this.description = description
+    }
 }
 
 enum class Gender {
@@ -19,4 +30,8 @@ enum class Gender {
     FEMALE
 }
 
-fun Person.pronoun(): String = if (gender == Gender.MALE) "he" else "she"
+private fun Person.pronoun(): String = if (gender == Gender.MALE) "he" else "she"
+
+private fun Person.print() = println("The name of the person is $name, ${pronoun()}" +
+        " is $age then ${pronoun()} ${if (isAgeOver18) "is" else "not is"} over 18," +
+        " ${description ?: "(no description)"}")
